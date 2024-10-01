753b5427353ae2953cb11a5d0c662a697bde9cae
a22jhepincre <114077543+a22jhepincre@users.noreply.github.com>
2024-10-01 20:32:10 +0200

    ranking in progress

--- web/js/ranking.js
+++ web/js/ranking.js
@@ -15,11 +15,19 @@ function getRanking(){
     .then(data => {
         console.log(data)
         let html = ``;
-        data['users'].forEach(user => {
+        let icon = "";
+        data['users'].forEach((user, key) => {
+            if(key==0) icon = `<i class="bi bi-star-fill fs-1" style="color:#fff633"></i>`;
+            else if(key==1) icon = `<i class="bi bi-star-fill fs-1" style="color:#c5c5c5"></i>`;
+            else if(key==2) icon = `<i class="bi bi-star-fill fs-1" style="color:#CD8032"></i>`;
+            else icon = key;
             html += `
                 <div class="card">
                     <div class="card-body">
-                        <div class="d-flex align-items-center justify-content-center">
+                        <div class="d-flex align-items-center justify-content-start">
+                            <div class="fw-bold fs-2 me-2">
+                                ${icon}
+                            </div>
                             <div class="me-2" style="border-radius: 100px; width:60px; height: 6 0px; overflow: hidden;">
                                 <img src="${user['profile_pic']}" alt="Profile Picture"
                                 style="width: 100%; height: 100%; object-fit: cover;">
